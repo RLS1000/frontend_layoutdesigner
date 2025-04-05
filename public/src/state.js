9b16034324c255fe55398195b849151df87b0a99
Mr. Knips <MrKnips@MBP-von-Ryan.fritz.box>
2025-04-05 14:26:11 +0200

    Add setCanvasDefaults function to state.js

--- public/src/state.js
+++ public/src/state.js
@@ -1,4 +1,5 @@
-// state.js
+// src/state.js
+
 export const appState = {
   variables: {
     name1: "ERIKA",
@@ -27,3 +28,15 @@ export const appState = {
     hasRestoredPresets: false
   }
 };
+
+/**
+ * Setzt die Standardwerte für die Canvas-Konfiguration im appState.
+ * Kann z. B. in main.js beim Initialisieren aufgerufen werden.
+ * @param {Object} defaults - Optionales Konfig-Objekt zum Überschreiben.
+ */
+export function setCanvasDefaults(defaults = {}) {
+  appState.canvas.scaleX = defaults.scaleX ?? 1;
+  appState.canvas.scaleY = defaults.scaleY ?? 1;
+  appState.canvas.logicalWidth = defaults.logicalWidth ?? 600;
+  appState.canvas.logicalHeight = defaults.logicalHeight ?? 400;
+}
